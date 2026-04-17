5fe3dc81390d2fe228337a75ba09ed72ba6cdfd0
lucrebel <lucrebel@github.com>
2026-04-17 16:23:26 +0200

    Update distribution plots for robust and standard scaling

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 
 # sklearn imports...
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.utils.fixes import parse_version
 
 # SVRs are not allowed in this project.
 #WICHTIG ist für Erklärungsauftrag
@@ -93,7 +95,7 @@ if __name__ == "__main__":
     #HOW THE DATASET LOOKS LIKE
     #print_dataset_plot()
 
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42,shuffle=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42,shuffle=True)
 
     #region Preprocessing Data
     """
@@ -185,30 +187,51 @@ if __name__ == "__main__":
 
     #endregion
     #endregion
-    
+
+
+    params = {
+        "max_depth": [3,6,8],
+        "min_samples_leaf": [2,4,8],
+        "learning_rate": [0.01,0.03],
+        "loss": ["squared_error"],
+        "max_iter": [100*y for y in range(6)],
+        "random_state": [0],
+    }
 
 
 
-    from sklearn.linear_model import ElasticNet
-    from sklearn.datasets import make_regression
+    from sklearn import ensemble
 
-    model = ElasticNet(alpha=1,max_iter=1000,random_state=42)
-    model.fit(X_train,y_train)
+    model = ensemble.HistGradientBoostingRegressor()
+    # Train on preprocessed features.
+    from sklearn.model_selection import GridSearchCV
+
+    grid_search = GridSearchCV(
+        model,
+        params,
+        cv=3,
+        verbose=3,
+        n_jobs=-1,
+        scoring="neg_mean_absolute_error"
+        )
+
+    grid_search.fit(X_train_scaled_robust,y_train)
+
 
 
 
 #region DON'T REMOVE -> GRADING SIMULATION!!!!!!###
     
     from sklearn.metrics import mean_absolute_error
-    y_pred = model.predict(X_test_scaled_robust)
+    y_pred = grid_search.predict(X_test_scaled_robust)
     
     mae = mean_absolute_error(y_test, y_pred)
-    print(mae)
+    print(f"{mae:.4f}")
 
     # Save Kaggle submission using the test split
-    test_images = load_test_dataset(config)
+    test_images = np.asarray(load_test_dataset(config), dtype=float)
     test_images_scaled = robust_scaler.transform(test_images)
-    test_pred = model.predict(test_images)
+    test_pred = grid_search.predict(test_images_scaled)
     save_results(test_pred)
 
 #endregion
